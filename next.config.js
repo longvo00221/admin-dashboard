8dd107d7af46fcf542622a8d59d473614445bf00
longvo00221 <longvo010203@gmail.com>
2024-01-20 13:17:54 +0700

    Fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images : {
+    images: {
         domains: [
             "res.cloudinary.com",
             "/"
         ]
-    }
+    },
+    output: {
+        // options related to "next export" go here
+    },
+    // other Next.js config options go here
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
